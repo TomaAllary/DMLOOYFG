917a7e0f036bad30fc79257e380736047326d735
allaryt <poki114poki114@outlook.com>
2022-01-30 13:55:27 -0500

    changed drawing bg color

--- DrawMeLikeOneOfYourFrenchGoats/Assets/Scripts/KillSwitch.cs
+++ DrawMeLikeOneOfYourFrenchGoats/Assets/Scripts/KillSwitch.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class KillSwitch : MonoBehaviour
 {
@@ -13,10 +14,27 @@ public class KillSwitch : MonoBehaviour
     public string nextScene;
     public int thisLvlIndex;
 
+    public RawImage overlay;
+
+    private Color overlayColor;
+    private float fadeOut = 3;
+    private bool hasWon = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        overlayColor = overlay.color;
+    }
+
+    private void Update() {
+        if (hasWon) {
+            if (fadeOut > 0) {
+                fadeOut -= Time.deltaTime;
+                overlayColor.a = 255;
+                overlay.color = overlayColor;
+            }
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -30,12 +48,13 @@ public class KillSwitch : MonoBehaviour
 
     IEnumerator NextScene() {
         if (nextScene != "") {
+            hasWon = true;
             //GameObject.Find("bg music").GetComponent<AudioSource>().volume = 0.1f;
 
             //add progress... 
             if (thisLvlIndex < 10)
                 File.AppendAllText(Application.dataPath + "/SaveProgress.txt", (thisLvlIndex + 1).ToString(), Encoding.ASCII);
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(3.4f);
 
             SceneManager.LoadScene(nextScene);
         }
